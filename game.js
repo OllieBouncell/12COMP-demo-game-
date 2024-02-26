79a6971bb197218b6ff97e5dd63c6760ac6a6a25
OllieBouncell <124744251+OllieBouncell@users.noreply.github.com>
2024-02-27 10:50:23 +1300

    Update game.js
    Movement Done

--- game.js
+++ game.js
@@ -5,8 +5,8 @@
 /*******************************************************/
 console.log("%c t01_create_sprite", "color: blue;");
 
-const SCREEN_WIDTH = 500;
-const SCREEN_HEIGHT = 250;
+const SCREEN_WIDTH = 400;
+const SCREEN_HEIGHT = 200;
 const PLAYER_WIDTH = 25;
 const PLAYER_HEIGHT = 25;
 
@@ -16,34 +16,27 @@ const PLAYER_HEIGHT = 25;
 /*******************************************************/
 function setup() {
   console.log("setup: ");
-  world.gravity.y = 10;
+  world.gravity.y = 80;
  cnv = new Canvas(SCREEN_WIDTH ,SCREEN_HEIGHT );
- player = new Sprite(PLAYER_HEIGHT, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd'); 
+ player = new Sprite(PLAYER _HEIGHT, SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd'); 
  player .color = 'pink';
-    wall = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT,  SCREEN_WIDTH, 5, 'k'); 
+ 
+ wall = new Sprite(SCREEN_WIDTH/2, SCREEN_HEIGHT,  SCREEN_WIDTH, 5, 'k'); 
   wall .color = 'pink';
   
-  
-document.addEventListener("keydown", function(event) {
 
-  if (event.code === 'ArrowUp') {
-player.vel.y = 50;
-    // Set sprite's velocity to the left
+document.addEventListener("keydown", 
+        function(event) {
+                        console.log("Key pressed!"+player.y);
 
-  }
-  });
- 
-document.addEventListener("keyup", function(event) {
+            if(player.y > 184 ){
+            console.log("Key pressed!");
+            player.vel.y = -20;
+            }
+    });
 
-  if (event.code === 'ArrowUp') {
-player.vel.y = 0;
-    // Set sprite's velocity to 0
 
-  }
- 
-});  
-  
-}
+    }
 
 /*******************************************************/
 // draw()
