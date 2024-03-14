6d7f736958ef85b75407a83df342334d38609954
OllieBouncell <124744251+OllieBouncell@users.noreply.github.com>
2024-03-14 15:11:58 +1300

    Update game.js
    added objects that fly at you and lives

--- game.js
+++ game.js
@@ -1,137 +1,161 @@
-/*******************************************************/
-// P5.play: t01_create_sprite
-// Create a sprite
-// Written by ??? 
-/*******************************************************/
 console.log("%c t01_create_sprite", "color: blue;");
 
+/*******************************************************/
+// variables()
+/*******************************************************/
+
 const SCREEN_WIDTH = 400;
-const SCREEN_HEIGHT = 200;
-const PLAYER_HEIGHT = 25;
-const PLAYER_WIDTH = 25;
+const SCREEN_HEIGHT = 200;;
+const OBSTACLE_HEIGHT = 25;
+const OBSTACLE_WIDTH = 25;
 
-
-const OBSTACLE_HEIGHT = PLAYER_HEIGHT;
-const OBSTACLE_WIDTH = PLAYER_WIDTH;
 
 var spawnDist = 0;
 var nextSpawn = 0;
-var score = 0;
-var player;
-  
-var screenSelector = "start";  
 
-var obstacles;
+var lives = 3;
 /*******************************************************/
 // setup()
 /*******************************************************/
-function setup() {
-    console.log("setup: ");
-    cnv= new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
-    
-    obstacles = new Group();
 
-    floor =  new Sprite(SCREEN_WIDTH/2,  SCREEN_HEIGHT, SCREEN_WIDTH, 4, 's');
-    floor.color = color("black");
-    world.gravity.y = 80;
-    
-    document.addEventListener("keydown", 
-        function(event) {
-            if(screenSelector == "start"||screenSelector == "end"){
-                screenSelector = "game"
-                resetGame();
-            }else{
-                if(player.y > 184 ){// 184 - found from testing - floor level
-                    console.log("Key pressed!");
-                    player.vel.y = -20;
-                }
-            }
-    });
+function preload() {
+imgPlayer = loadImage('/images/player.png');
 
 }
 
+
+
+
+
+function setup() {
+    console.log("setup: ");
+    
+    cnv= new Canvas(SCREEN_WIDTH, SCREEN_HEIGHT);
+    fill('red');
+     obstacles = new Group();
+
+    wallLH  = new Sprite(0, SCREEN_HEIGHT/2, 8, SCREEN_HEIGHT , 's');
+    wallLH.color = 'lightblue';
+    WallLH = noStroke()
+ 
+    
+    wallTop = new Sprite(SCREEN_WIDTH/2, 0,SCREEN_WIDTH, 8, 's');
+    wallTop.color = 'lightblue';
+    WallTop = noStroke()
+  
+    
+    wallBot = new Sprite(SCREEN_WIDTH/2,  SCREEN_HEIGHT , SCREEN_WIDTH, 8, 's ');
+    wallBot.color = 'lightblue';
+    WallBot = noStroke()
+   
+ 
+ 
+ 
+ 
+ 
+ //P5.play website//
+    player = new Sprite(100, 100, 20, 'd');
+    player.color = 'green';
+ 
+     player.addImage(imgPlayer);
+    imgPlayer.resize(100, 100);
+ 
+
+
+ 
+ 
+ 
+ // Keyboard Movement-Up and Down //
+document.addEventListener("keydown", function(event) {
+
+  if (event.code === 'ArrowLeft') {
+player.vel.x = -3;
+    // Set sprite's velocity to the left
+
+  }
+
+ else if 
+(event.code === 'ArrowRight')  player.vel.x = 3;
+else if 
+(event.code === 'ArrowUp') player.vel.y = -3;
+else if 
+(event.code === 'ArrowDown')  player.vel.y = 3; 
+
+
+});
+
+
+
+document.addEventListener("keyup", function(event) {
+
+  if (event.code === 'ArrowLeft') {
+player.vel.x = 0;
+    // Set sprite's velocity to 0
+
+  }
+
+ else if 
+(event.code === 'ArrowRight')  player.vel.x = 0;
+else if 
+(event.code === 'ArrowUp')  player.vel.y = 0;
+else if 
+(event.code === 'ArrowDown')  player.vel.y = 0;
+
+obstacles.collides(player, playerHitAlien);
+
+});
+
+
+
+}
 /*******************************************************/
 // draw()
 /*******************************************************/
 function draw() {
-    if(screenSelector=="game"){
-        gameScreen();
-    }else if(screenSelector=="end"){
-        endScreen();
-    }else if(screenSelector=="start"){
-        startScreen();
-    }else{
-        text("wrong screen", 50, 50);
-        console.log("wrong screen ")
+ background("lightblue");
+ player.rotation = 0;
+ player.bounciness = 0;
+ allSprites.visible = true;
+     for (i = 0; i <lives; i++){
+    rect(40 * i, 10, 35, 35);
+  }
+
+
+   if(frameCount> nextSpawn){
+        newObstacle();
+        nextSpawn = frameCount + random(10,200);
     }
 }
 
+
+ 
+  
+
+
+
 function newObstacle(){
-    obstacle = new Sprite((SCREEN_WIDTH -100),  SCREEN_HEIGHT - OBSTACLE_HEIGHT/2, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
+    // Code from Geodash
+    var screenY = random(15,200)
+    obstacle = new Sprite(SCREEN_WIDTH, screenY, OBSTACLE_WIDTH, OBSTACLE_HEIGHT, 'k');
     obstacle.color = color("green");
-    obstacle.vel.x = -10;
+    obstacle.vel.x = -1;
     
     obstacles.add(obstacle);
 }
 
-function youDied(_player, _obstacle){
-    screenSelector = "end";
-    player.remove();
-    obstacles.removeAll();
+
+
+function playerHitAlien (_player,alien) {
+
+    lives --;
+    console.log("Lives");
+ 
+ 
+ 
+ 
 }
 
-// Main screen functions
 
-function startScreen(){
-    background("white");
 
-    allSprites.visible = false;
-    textSize(32);
-    fill(255);
-    stroke(0);
-    strokeWeight(4);
-    text("GEODASH", 50, 50);
-    textSize(24);
-    text("Press to start", 50, 110);
-}
 
-function gameScreen(){
-    background("lightblue");
-    allSprites.visible = true;
-    score++;
-    if(frameCount> nextSpawn){
-        newObstacle();
-        nextSpawn = frameCount + random(10,100);
-    }
-    textSize(32);
-    fill(255);
-    stroke(0);
-    strokeWeight(4);
-    text(score, 50, 50);
-}
 
-function endScreen(){
-    background("white");
-
-    allSprites.visible = false;
-    textSize(32);
-    fill(255);
-    stroke(0);
-    strokeWeight(4);
-    text("You died", 50, 50);
-    textSize(24);
-    text("your score was: "+score, 50, 110);
-    textSize(14);
-    text("press any key to play again", 50, 150);
-}
-
-function resetGame(){
-    player = new Sprite(PLAYER_WIDTH*1.2,  SCREEN_HEIGHT/2, PLAYER_WIDTH, PLAYER_HEIGHT, 'd');
-    player.color = color("pink");
-    player.collides(obstacles, youDied);
-    score = 0;
-}
-
-/*******************************************************/
-//  END OF APP
-/*******************************************************/
